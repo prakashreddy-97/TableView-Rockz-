af1d78f875007ac77223b0b922cca427c4dd14d7
Student <student@ol10037m.nwmissouri.edu>
2019-02-20 19:25:17 -0600

    added second view

--- TableView Rockz!/FirstViewController.swift
+++ TableView Rockz!/FirstViewController.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
 
+class FirstViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0{
+            return cities.count
+        }else{
+            return -1
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cities")!
+        cell.textLabel?.text = cities[indexPath.row]
+        return cell
+    }
+    
+    var cities = ["Paris","London","Adelaide", "New York", "Hyderabad"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    
 }
 
